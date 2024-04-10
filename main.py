79d224b83036692addc269ffc8c617d191a4aa90
muhammetguducu <84397069+MuhammetGuducu@users.noreply.github.com>
2024-04-10 11:32:13 +0200

    Usage of Pylint
    Improved code quality by using Pylint. Additionally, translated all german comments to english.

--- main.py
+++ main.py
@@ -1,55 +1,61 @@
-import tkinter as tk
-import requests
+"""
+This module provides functionality for sending weather reports via email
+based on the current weather conditions at a specified location.
+"""
+
+import os
 import smtplib
 import configparser
-from cryptography.fernet import Fernet
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
+import tkinter as tk
+import requests
+from cryptography.fernet import Fernet
 
 
-#conditions.txt, welche Kleidung empfohlen wird
 def conditions(main):
+    """Determine clothing suggestions based on weather conditions."""
     if main in ["Rain", "Drizzle", "Thunderstorm"]:
         return "Kopfbedeckung und "
-    elif main in ["Snow"]:
+    if main in ["Snow"]:
         return "Handschuhe, Kopfbedeckung und "
-    else:
-        return ""
+    return ""
 
 
-# Clothing.txt, welche Kleidung empfohlen wird
 def clothing(temperature, cold_threshold, warm_threshold):
+    """Determine clothing suggestions based on temperature."""
     if temperature > int(warm_threshold):
         return "luftige Kleidung"
-    elif temperature < int(cold_threshold):
+    if temperature < int(cold_threshold):
         return "warme Kleidung"
-    else:
-        return "normale Kleidung"
+    return "normale Kleidung"
 
-# Verbindet sich mit OpenWeatherMap um Echtzeitwetterdaten zu sammeln
-def getData(api_key, lat, lon, lang):
+
+def get_data(api_key, lat, lon, lang):
+    """Retrieve weather data from OpenWeatherMap API."""
     request_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&lang={lang}"
-    feedback = requests.get(request_url)
-    if feedback.status_code == 200: # Wenn Verbindung erfolgreich, dann Wetter Daten in json format zurückgeben
+    feedback = requests.get(request_url, timeout=10)
+    if (
+        feedback.status_code == 200
+    ):  # If connection is successful, return the collected data
         return feedback.json()
-    else:
-        return None
+    return None
 
-# Sendet die Wetterberichts E-Mail
-def sendEmail(data, sender, receiver, password, cold_threshold, warm_threshold):
+
+def send_email(data, sender, receiver, password, cold_threshold, warm_threshold):
+    """Send email with weather report."""
     if data:
-        region = data["name"] ## Ruhrort
-        description = data["weather"][0]["description"] ## Leichter Regen
+        region = data["name"]
+        description = data["weather"][0]["description"]
         temp = round(data["main"]["temp"] - 273.15)
         main = data["weather"][0]["main"]
-        today = datetime.now().strftime("%d.%m.%y")
+        today = datetime.datetime.now().strftime("%d.%m.%y")
 
         conditions_txt = conditions(main)
         clothing_txt = clothing(temp, cold_threshold, warm_threshold)
 
-
-        # Tabelle mit den Daten schön formattiert
+        #
         html_body = f"""
         <html>
             <head>
@@ -80,188 +86,205 @@ def sendEmail(data, sender, receiver, password, cold_threshold, warm_threshold):
         </html>
         """
 
-        msg = MIMEMultipart("alternative") ## E-Mail Darstellung aus mehreren Teilen, da wir Text+HTML haben
+        msg = MIMEMultipart("alternative")  # Both Text + HTML content combined
         msg["From"] = sender
         msg["To"] = receiver
         msg["Subject"] = f"Wetterbericht für {region} am {today}"
         msg.attach(MIMEText(html_body, "html"))
 
-        try: ## Da beim Server Verbindungsaufbau fehler passieren können, zb. passwort falsch oder 2-FA Authentifizierung
-            with smtplib.SMTP("smtp.gmail.com", 587) as server: # Verbindung zum Server mit TLS-Verchlüsselung
+        try:  # Server connection test, prints clear description of errors
+            with smtplib.SMTP("smtp.gmail.com", 587) as server:
                 server.starttls()
                 server.login(sender, password)
                 server.sendmail(sender, receiver, msg.as_string())
-                server.quit()
-                print("Mail erfolgreich gesendet!")
-        except Exception as e:
+        except smtplib.SMTPException as e:
             print(f"Fehler beim Senden der Mail: {e}")
 
 
-# Erstellt eine Standardkonfigurationsdatei, falls keine vorhanden ist oder wenn der Fernet-Key ungültig ist
-def createDefaultConfig():
+def create_default_config():
+    """Create a default configuration file if none is available."""
     config = configparser.ConfigParser()
 
-    config['WEATHER'] = {
-        'latitude': '51.4344',
-        'longitude': '6.7623',
-        'language': 'de',
-        'cold_threshold': '5',
-        'warm_threshold': '20'
+    config["WEATHER"] = {
+        "latitude": "51.4344",
+        "longitude": "6.7623",
+        "language": "de",
+        "cold_threshold": "5",
+        "warm_threshold": "20",
     }
 
-    config['EMAIL'] = {
-        'sender_email': 'sender@gmail.com',
-        'encrypted_password': '',
-        'receiver_email': 'receiver@gmail.com'
+    config["EMAIL"] = {
+        "sender_email": "sender@gmail.com",
+        "encrypted_password": "",
+        "receiver_email": "receiver@gmail.com",
     }
 
-    with open('config.ini', 'w') as configfile:
+    with open("config.ini", "w", encoding="utf-8") as configfile:
         config.write(configfile)
 
 
-# Falls der Nutzer auf "Eingaben speichern" und "Senden" klickt, werden alle eingegebenen Daten in die config.ini geschrieben
-def saveConfig(lat, lon, lang, cold_threshold, warm_threshold, sender, receiver, password, key):
+def save_config(
+    lat, lon, lang, cold_threshold, warm_threshold, sender, receiver, password, key
+):
+    """
+    Save the current configuration to config.ini if user allows it.
+    """
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
 
-    config.set('WEATHER', 'latitude', lat) # Falls bei Submit auf Eingaben speichern geklickt wurde, dann sollen die Eingaben in config geschrieben werden
-    config.set('WEATHER', 'longitude', lon)
-    config.set('WEATHER', 'language', lang)
-    config.set('WEATHER', 'cold_threshold', str(cold_threshold)) # Umwandlung in String, da configparser.set kein value returnt
-    config.set('WEATHER', 'warm_threshold', str(warm_threshold))
-    config.set('EMAIL', 'sender_email', sender)
-    config.set('EMAIL', 'receiver_email', receiver)
+    config.set("WEATHER", "latitude", lat)
+    config.set("WEATHER", "longitude", lon)
+    config.set("WEATHER", "language", lang)
+    config.set("WEATHER", "cold_threshold", str(cold_threshold))
+    config.set("WEATHER", "warm_threshold", str(warm_threshold))
+    config.set("EMAIL", "sender_email", sender)
+    config.set("EMAIL", "receiver_email", receiver)
 
-    # Der schlüssel aus passwordKey.txt wird verschlüsselt im lokalen Objekt cipher gespeichert. Bei jeder verschlüsselung und entschlüsselung braucht man cipher
-    # Kodiert das Passwort in Bytes (encode), dann werden die Bytes verschlüsselt mit encrypt
-    # Dekodiert das Passwort zurück um einen String aus Bytes zu erhalten, damit es in der config.ini gespeichert werden kann
+    # Encrypted Binary Key from passwordKey.txt saved in "cipher"
+    # Password-String into Bytes, encrypts them and decodes back into string
     cipher = Fernet(key)
-    encrypted_password = cipher.encrypt(password.encode())
-    config.set('EMAIL', 'encrypted_password', encrypted_password.decode())
+    encrypted_password = cipher.encrypt(password.encode()).decode()
+    config.set("EMAIL", "encrypted_password", encrypted_password)
 
-    with open('config.ini', 'w') as configData: # Write um alle configs die wir vorher gesetzt haben in config.ini reinzuschreiben
-        config.write(configData)
+    with open("config.ini", "w", encoding="utf-8") as config_data:
+        config.write(config_data)
 
 
-# Inhalte der Eingabefelder werden aus der config.ini geladen
-def loadConfig():
-    config = configparser.ConfigParser() #configparser bibliothek um ini dateien lesen/schreiben
-    import os
-    if not os.path.exists('config.ini'):
-        createDefaultConfig()
-    config.read('config.ini')
+def load_config():
+    """Load configuration from a file."""
+    config = configparser.ConfigParser()  # used to write/read ini files
+    if not os.path.exists("config.ini"):
+        create_default_config()
+    config.read("config.ini")
 
-    with open('passwordKey.txt', 'rb') as passwordKey: # read-binary
-        key = passwordKey.read()
+    with open("passwordKey.txt", "rb") as password_key:  # rb = read-binary
+        key = password_key.read()
 
     cipher = Fernet(key)
-    encrypted_password = config.get('EMAIL', 'encrypted_password')
-    decrypted_password = cipher.decrypt(encrypted_password.encode()).decode() if encrypted_password else "" # Falls kein Passwort vorhanden, nur ein leeren String zurückgeben
-    # Verschlüsseltes Password wird entschlüsselt, in Bytes umgewandelt zur Verschlüsselung (da es als string gespeichert war), dann werden die Bytes entschlüsselt.
+    encrypted_password = config.get("EMAIL", "encrypted_password")
+    decrypted_password = (
+        cipher.decrypt(
+            encrypted_password.encode()
+        ).decode()  # string into bytes, decrypt bytes and back into string
+        if encrypted_password
+        else ""  # return empty string if there is no password
+    )
 
     return {
-        'latitude': config.get('WEATHER', 'latitude'),
-        'longitude': config.get('WEATHER', 'longitude'),
-        'language': config.get('WEATHER', 'language'),
-        'cold_threshold': int(config.get('WEATHER', 'cold_threshold')),
-        'warm_threshold': int(config.get('WEATHER', 'warm_threshold')),
-        'sender_email': config.get('EMAIL', 'sender_email'),
-        'receiver_email': config.get('EMAIL', 'receiver_email'),
-        'decrypted_password': decrypted_password
+        "latitude": config.get("WEATHER", "latitude"),
+        "longitude": config.get("WEATHER", "longitude"),
+        "language": config.get("WEATHER", "language"),
+        "cold_threshold": int(config.get("WEATHER", "cold_threshold")),
+        "warm_threshold": int(config.get("WEATHER", "warm_threshold")),
+        "sender_email": config.get("EMAIL", "sender_email"),
+        "receiver_email": config.get("EMAIL", "receiver_email"),
+        "decrypted_password": decrypted_password,
     }
 
 
-# Hier wird das GUI erstellt, die Daten werden gesammelt um die Email zu senden
-def input():
-    def submitButton():
+def user_input():
+    """Capture user input for the application and send the email."""
 
+    def submit_button():
+        lat = lat_input.get()
+        lon = lon_input.get()
+        lang = lang_input.get()
+        cold_threshold = cold_threshold_input.get()
+        warm_threshold = warm_threshold_input.get()
+        sender = sender_input.get()
+        receiver = receiver_input.get()
+        password = password_input.get()
 
-        lat = latInput.get()
-        lon = lonInput.get()
-        lang = langInput.get()
-        coldThreshold = coldThresholdInput.get()
-        warmThreshold = warmThresholdInput.get()
-        sender = senderInput.get()
-        receiver = receiverInput.get()
-        password = passwordInput.get()
+        if save_config_var.get():
+            save_config(
+                lat,
+                lon,
+                lang,
+                cold_threshold,
+                warm_threshold,
+                sender,
+                receiver,
+                password,
+                password_key,
+            )
 
-        if saveConfigVar.get():
-            saveConfig(lat, lon, lang, coldThreshold, warmThreshold, sender, receiver, password, passwordKey)
-
-        data = getData(apiKey, lat, lon, lang)
-        sendEmail(data, sender, receiver, password, coldThreshold, warmThreshold)
+        data = get_data(API_KEY, lat, lon, lang)
+        send_email(data, sender, receiver, password, cold_threshold, warm_threshold)
         window.destroy()
 
-    configValues = loadConfig()
+    config_values = load_config()
 
-
-    window = tk.Tk() # Haupt-Fenster für die Eingabe wird gestartet
+    window = tk.Tk()
     window.title("WetterMail")
     window.geometry("360x230")
-    inputWidth = 35
+    input_width = 35
 
-    tk.Label(window, text="Breitengrad:").grid(row=1, column=0) # Text "Breitengrad" soll auf Zeile 1, Spalte 0 sein
-    latInput = tk.Entry(window, width=inputWidth) #Eingabefeld
-    latInput.insert(0, configValues['latitude']) #Eingabefeld erhält vordefinierten Wert aus config.ini
-    latInput.grid(row=1, column=1) # Eingabefeld soll auf Zeile 1, Spalte 1 positioniert sein
+    # GUI setup code for the user input
+    tk.Label(window, text="Breitengrad:").grid(row=1, column=0)
+    lat_input = tk.Entry(window, width=input_width)
+    lat_input.insert(0, config_values["latitude"])
+    lat_input.grid(row=1, column=1)
 
     tk.Label(window, text="Längengrad:").grid(row=2, column=0)
-    lonInput = tk.Entry(window, width=inputWidth)
-    lonInput.insert(0, configValues['longitude'])
-    lonInput.grid(row=2, column=1)
+    lon_input = tk.Entry(window, width=input_width)
+    lon_input.insert(0, config_values["longitude"])
+    lon_input.grid(row=2, column=1)
 
     tk.Label(window, text="Sprache (de/en):").grid(row=3, column=0)
-    langInput = tk.Entry(window, width=inputWidth)
-    langInput.insert(0, configValues['language'])
-    langInput.grid(row=3, column=1)
+    lang_input = tk.Entry(window, width=input_width)
+    lang_input.insert(0, config_values["language"])
+    lang_input.grid(row=3, column=1)
 
     tk.Label(window, text="Kälteschwelle:").grid(row=4, column=0)
-    coldThresholdInput = tk.Entry(window, width=inputWidth)
-    coldThresholdInput.insert(0, configValues['cold_threshold'])
-    coldThresholdInput.grid(row=4, column=1)
+    cold_threshold_input = tk.Entry(window, width=input_width)
+    cold_threshold_input.insert(0, config_values["cold_threshold"])
+    cold_threshold_input.grid(row=4, column=1)
 
     tk.Label(window, text="Wärmeschwelle:").grid(row=5, column=0)
-    warmThresholdInput = tk.Entry(window, width=inputWidth)
-    warmThresholdInput.insert(0, configValues['warm_threshold'])
-    warmThresholdInput.grid(row=5, column=1)
+    warm_threshold_input = tk.Entry(window, width=input_width)
+    warm_threshold_input.insert(0, config_values["warm_threshold"])
+    warm_threshold_input.grid(row=5, column=1)
 
     tk.Label(window, text="Absender E-Mail:").grid(row=6, column=0)
-    senderInput = tk.Entry(window, width=inputWidth)
-    senderInput.insert(0, configValues['sender_email'])
-    senderInput.grid(row=6, column=1)
+    sender_input = tk.Entry(window, width=input_width)
+    sender_input.insert(0, config_values["sender_email"])
+    sender_input.grid(row=6, column=1)
 
     tk.Label(window, text="Absender Passwort:").grid(row=7, column=0)
-    passwordInput = tk.Entry(window, show="*", width=inputWidth)
-    passwordInput.insert(0, configValues['decrypted_password'])
-    passwordInput.grid(row=7, column=1)
+    password_input = tk.Entry(window, show="*", width=input_width)
+    password_input.insert(0, config_values["decrypted_password"])
+    password_input.grid(row=7, column=1)
 
     tk.Label(window, text="Empfänger E-Mail:").grid(row=8, column=0)
-    receiverInput = tk.Entry(window, width=inputWidth)
-    receiverInput.insert(0, configValues['receiver_email'])
-    receiverInput.grid(row=8, column=1)
+    receiver_input = tk.Entry(window, width=input_width)
+    receiver_input.insert(0, config_values["receiver_email"])
+    receiver_input.grid(row=8, column=1)
 
-    saveConfigVar = tk.IntVar()
-    saveConfigCheckbutton = tk.Checkbutton(window, text="Eingaben speichern", variable=saveConfigVar)
-    saveConfigCheckbutton.grid(row=9, column=0)
+    save_config_var = tk.IntVar()
+    save_config_checkbutton = tk.Checkbutton(
+        window, text="Eingaben speichern", variable=save_config_var
+    )
+    save_config_checkbutton.grid(row=9, column=0)
 
-    submitButton = tk.Button(window, text="Senden", command=submitButton)
-    submitButton.grid(row=10, column=0, columnspan=2)
+    submit_button = tk.Button(window, text="Senden", command=submit_button)
+    submit_button.grid(row=10, column=0, columnspan=2)
 
-    window.mainloop() # Reagiert auf Benutzereingaben, aktualisiert die GUI bis es beendet wird durch destroy
+    window.mainloop()  # GUI Reacts to user interaction like mouse clicks
 
 
-# Key-Erstellung und Überprüfung
 if __name__ == "__main__":
-    apiKey = "e055a71967956ab0d652f985f92e79a4"
+    API_KEY = "e055a71967956ab0d652f985f92e79a4"
     try:
-        with open("passwordKey.txt", "r") as file:
-            passwordKey = file.read()
-            Fernet(passwordKey)  # Erstelle Fernet objekt um zu überprüfen ob der Schlüssel gültig ist
-    except (FileNotFoundError, ValueError): # Datei existiert nicht oder Schlüssel ist ungültig
-        passwordKey = Fernet.generate_key().decode()
-        createDefaultConfig()
+        with open("passwordKey.txt", "r", encoding="utf-8") as file:
+            password_key = file.read()
+            Fernet(
+                password_key
+            )  # Create Fernet-Object to check if PasswordKey is valid
+    except (FileNotFoundError, ValueError):
+        password_key = Fernet.generate_key().decode()
+        create_default_config()
         print("Ungültiger Fernet Key, alle Konfigurationsdaten wurden zurückgesetzt!")
-        with open("passwordKey.txt", "w") as file:
-            file.write(passwordKey) # Schreibt einen neuen Key in passwordKey.txt
+        with open("passwordKey.txt", "w", encoding="utf-8") as file:
+            file.write(password_key)  # if not valid, write a valid passwordKey
 
-    input()
+    user_input()
